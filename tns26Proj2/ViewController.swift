1d869813812980b44f6935940387d3f268c0b36c
tstaten56 <tstaten56@gmail.com>
2015-10-13 12:20:43 -0400

    first commit

---
+++ tns26Proj2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tns26Proj2
+//
+//  Created by Staten,Tera on 10/13/15.
+//  Copyright © 2015 Staten,Tera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
