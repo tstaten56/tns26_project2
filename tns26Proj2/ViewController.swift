fa30ce96d6b4f83debdcf8d4ac69ac9063df7c56
tstaten56 <tstaten56@gmail.com>
2015-10-13 16:52:57 -0400

    This code is broken, just looking at it for reading from a file

--- tns26Proj2/ViewController.swift
+++ tns26Proj2/ViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+let Birthday = "Birthday"
+let Name = "Name"
+let Spouse = "Spouse"
+let Roles = "Roles"
+let Awards = "Awards"
 
 class ViewController: UIViewController {
 
@@ -25,6 +30,41 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loadData() {
+        // getting path to GameData.plist
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as NSArray
+        let documentsDirectory = paths[0] as! String
+        let path = documentsDirectory.smallestEncoding
+        let fileManager = NSFileManager.defaultManager()
+        //check if file exists
+        if(!fileManager.fileExistsAtPath(path)) {
+            // If it doesn't, copy it from the default file in the Bundle
+            if let bundlePath = NSBundle.mainBundle().pathForResource("GameData", ofType: "plist") {
+                let resultDictionary = NSMutableDictionary(contentsOfFile: bundlePath)
+                println("Bundle GameData.plist file is --> \(resultDictionary?.description)")
+                fileManager.copyItemAtPath(bundlePath, toPath: path, error: nil)
+                println("copy")
+            } else {
+                println("GameData.plist not found. Please, make sure it is part of the bundle.")
+            }
+        } else {
+            println("GameData.plist already exits at path.")
+            // use this to delete file from documents directory
+            //fileManager.removeItemAtPath(path, error: nil)
+        }
+        let resultDictionary = NSMutableDictionary(contentsOfFile: path)
+        println("Loaded GameData.plist file is --> \(resultDictionary?.description)")
+        var myDict = NSDictionary(contentsOfFile: path)
+        if let dict = myDict {
+            //loading values
+            bedroomFloorID = dict.objectForKey(BedroomFloorKey)!
+            bedroomWallID = dict.objectForKey(BedroomWallKey)!
+            //...
+        } else {
+            println("WARNING: Couldn't create dictionary from GameData.plist! Default values will be used!")
+        }
     }
 
 
