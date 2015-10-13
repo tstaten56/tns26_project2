5e86504c12d36278d7530efec1e8cd009e432402
tstaten56 <tstaten56@gmail.com>
2015-10-13 16:42:45 -0400

    more constraints, added some external refs for coding too

--- tns26Proj2/ViewController.swift
+++ tns26Proj2/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BdayLabel: UILabel!
+    @IBOutlet weak var NameLabel: UILabel!
+    @IBOutlet weak var SpouseLabel: UILabel!
+    @IBOutlet weak var RolesLabel: UILabel!
+    @IBOutlet weak var AwardsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NameLabel.text = "hello"
     }
 
     override func didReceiveMemoryWarning() {
